ee8ab803e48ebef14b70e76c3daca08c00f21d41
Ameen Ahmed <ameenahmed39@gmail.com>
2017-07-13 21:54:26 -0400

    Add method stubs to MusicApp.py

--- MusicApp.py
+++ MusicApp.py
@@ -1,2 +1,25 @@
 # Ameen Ahmed
 # 7/13/17
+
+from tkinter import *
+from tkinter import ttk
+
+
+class MusicApp:
+    def __init__(self):
+        pass
+    def __createGUI(self):
+        pass
+    def callback(self):
+        pass
+    def close(self):
+        pass
+
+
+def main():
+    root = Tk()
+    MusicApp(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    pass
