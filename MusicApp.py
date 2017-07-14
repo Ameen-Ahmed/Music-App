207960b1ccac119aa20fbd38612a6abb90f1bd42
Ameen Ahmed <ameenahmed39@gmail.com>
2017-07-14 19:05:47 -0400

    Create basic GUI skeleton
        - Create and configure Master Window
        - Create and configure Style object
        - Create upper entry_frame, and lower output_frame
        - Insert Entry widgets and Label widgets into entry_frame
        - Insert empty Label widgets into output_frame

--- MusicApp.py
+++ MusicApp.py
@@ -6,20 +6,72 @@ from tkinter import ttk
 
 
 class MusicApp:
-    def __init__(self):
-        pass
-    def __createGUI(self):
-        pass
-    def callback(self):
-        pass
-    def close(self):
-        pass
+    def __init__(self, master):
+        self.master = master
+        self.createGUI()
 
+    def createGUI(self):
+
+        # Configure master window
+        bkgrnd = '#FF6161'
+        self.master.configure(background=bkgrnd)
+        self.master.title('MusicApp.py')
+        self.master.resizable(False, False)
+
+        # Style
+        style = ttk.Style(self.master)
+        style.configure('.', background=bkgrnd, font=('Arial Black', 12))
+
+        # Frame creation
+        self.entry_frame = ttk.Frame(self.master)
+        self.entry_frame.pack()
+        self.output_frame = ttk.Frame(self.master)
+
+        # Entry_frame design
+        ttk.Label(self.entry_frame, text='Song').grid(
+            row=0, column=0, padx=100, sticky='w')
+        ttk.Label(self.entry_frame, text='Artist').grid(
+            row=0, column=2, padx=100, sticky='w')
+        self.song = StringVar()
+        self.artist = StringVar()
+        ttk.Entry(self.entry_frame, textvariable=self.song).grid(row=1, column=0)
+        ttk.Entry(self.entry_frame, textvariable=self.artist).grid(row=1, column=2)
+
+        ttk.Button(self.entry_frame, text='Search',
+                   command=self.callback).grid(row=2, column=1)
+
+
+
+        # Output_frame design
+        self.yt_link = StringVar()
+        self.sc_link = StringVar()
+        self.am_link = StringVar()
+        self.sp_link = StringVar()
+
+        self.yt_img = PhotoImage(file='')
+        self.sc_img = PhotoImage(file='')
+        self.am_img = PhotoImage(file='')
+        self.sp_img = PhotoImage(file='')
+
+        self.yt = ttk.Label(self.output_frame,
+                            textvariable=self.yt_link,
+                            compound=LEFT, image=self.yt_img).pack()
+        self.sc = ttk.Label(self.output_frame,
+                            textvariable=self.sc_link,
+                            compound=LEFT, image=self.sc_img).pack()
+        self.am = ttk.Label(self.output_frame,
+                            textvariable=self.am_link,
+                            compound=LEFT, image=self.am_img).pack()
+        self.sp = ttk.Label(self.output_frame,
+                            textvariable=self.sp_link,
+                            compound=LEFT, image=self.sp_img).pack()
+
+    def callback(self):
+        self.output_frame.pack()
 
 def main():
     root = Tk()
     MusicApp(root)
     root.mainloop()
 
-if __name__ == '__main__':
-    pass
+if __name__ == '__main__': main()
