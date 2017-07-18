1ae587bc8671c178109012852ad7d6bff5c63a22
Ameen Ahmed <ameenahmed39@gmail.com>
2017-07-18 12:12:26 -0400

    Add method bodies for Youtube/Audiomack searches

--- Web.py
+++ Web.py
@@ -1,11 +1,33 @@
-import urllib.request
+from bs4 import BeautifulSoup
+from urllib import request
+import re
+
+def youtube_search(song, artist):
+    format_song = song.replace(' ', '+')
+    format_song = format_song.strip()
+    format_artist = artist.replace(' ', '+')
+    format_artist = format_artist.strip()
+    data = request.urlopen('https://www.youtube.com/results?search_query=' + format_song + '+by+' + format_artist)
+    soup = BeautifulSoup(data)
+    for link in soup.findAll('a', attrs={'href': re.compile('^/watch')}):
+        return link['href']
 
 
-def youtube_search():
+
+def soundcloud_search(song, artist):
     pass
-def soundcloud_search():
+
+
+def spotify_search(song, artist):
     pass
-def spotify_search():
-    pass
-def audiomack_search():
-    pass
+
+
+def audiomack_search(song, artist):
+    format_song = song.replace(' ', '+')
+    format_song = format_song.strip()
+    format_artist = artist.replace(' ', '+')
+    format_artist = format_artist.strip()
+    data = request.urlopen('https://www.audiomack.com/search?q=' + format_song + '+' + format_artist)
+    soup = BeautifulSoup(data)
+    for link in soup.findAll('a', attrs={'href': re.compile('^/song/'+format_artist)}):
+        return 'https://www.audiomack.com' + link['href']
