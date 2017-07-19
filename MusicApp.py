529a8ef080bcdb7f77efe975018049daf626d28b
Ameen Ahmed <ameenahmed39@gmail.com>
2017-07-19 19:38:25 -0400

    Modify GUI formatting
        - Change background color
        - Change geometry manager for output_frame
        - Pack Search button between frames
        - Add labels used to adjust upper frame spacing
        - Modify some other spacing

--- MusicApp.py
+++ MusicApp.py
@@ -16,15 +16,15 @@ class MusicApp:
     def createGUI(self):
 
         # Configure master window
-        bkgrnd = '#FF6161'
+        bkgrnd = '#AE2D58'
         self.master.configure(background=bkgrnd)
         self.master.title('MusicApp.py')
         self.master.resizable(False, False)
 
         # Style
         style = ttk.Style(self.master)
-        style.configure('.', background=bkgrnd, font=('Arial Black', 10))
-        style.configure('Link.TLabel', font=('Arial', 10, 'bold'), fg='blue')
+        style.configure('.', background=bkgrnd, font=('Arial Black', 8))
+        style.configure('Link.TLabel', font=('Arial', 8, 'bold'), fg='blue')
 
         # Frame creation
         self.entry_frame = ttk.Frame(self.master)
@@ -33,19 +33,20 @@ class MusicApp:
 
         # Entry_frame design
         ttk.Label(self.entry_frame, text='Song').grid(
-            row=0, column=0, padx=100, sticky='w')
+            row=0, column=0, padx=75, sticky='w')
         ttk.Label(self.entry_frame, text='Artist').grid(
-            row=0, column=2, padx=100, sticky='w')
+            row=0, column=2, padx=75, sticky='w')
+        ttk.Label(self.entry_frame).grid(
+            row=0, column=1, padx=50, sticky='w')
 
         self.song = StringVar()
         self.artist = StringVar()
 
-        ttk.Entry(self.entry_frame, textvariable=self.song).grid(row=1, column=0)
-        ttk.Entry(self.entry_frame, textvariable=self.artist).grid(row=1, column=2)
-
-        ttk.Button(self.entry_frame, text='Search',
+        ttk.Entry(self.entry_frame, width=20, textvariable=self.song).grid(row=1, column=0)
+        ttk.Entry(self.entry_frame, width=20, textvariable=self.artist).grid(row=1, column=2)
+        ttk.Button(self.master, text='Search',
                    command=lambda: self.callback(
-                       self.song.get(), self.artist.get())).grid(row=2, column=1)
+                       self.song.get(), self.artist.get())).pack(pady=10)
 
 
         # Output_frame design
@@ -53,29 +54,33 @@ class MusicApp:
         self.sc_link = StringVar()
         self.am_link = StringVar()
 
-        self.yt_img = PhotoImage(file='icons/youtube-icon.png').subsample(5, 5)
-        self.sc_img = PhotoImage(file='icons/soundcloud-icon.png').subsample(12, 12)
-        self.am_img = PhotoImage(file='icons/audiomack-icon.png').subsample(3, 3)
+        self.yt_img = PhotoImage(file='icons/youtube-icon.png').subsample(6, 6)
+        ttk.Label(self.output_frame, image=self.yt_img).grid(row=0, column=0, pady=10)
 
+        self.sc_img = PhotoImage(file='icons/soundcloud-icon.png').subsample(14, 14)
+        ttk.Label(self.output_frame, image=self.sc_img).grid(row=1, column=0, pady=10)
+
+        self.am_img = PhotoImage(file='icons/audiomack-icon.png').subsample(3, 3)
+        ttk.Label(self.output_frame, image=self.am_img).grid(row=2, column=0, pady=10)
         self.yt = ttk.Label(self.output_frame, cursor='hand2',
-                            wraplength=500,
+                            wraplength=350,
                             textvariable=self.yt_link, style='Link.TLabel',
-                            compound=LEFT, image=self.yt_img)
-        self.yt.pack(anchor='w', pady=20)
+                            compound=LEFT)
+        self.yt.grid(row=0, column=1, sticky='w', pady=10)
         self.yt.bind('<Button-1>', lambda e: self.link_callback(e, self.yt_link))
 
         self.sc = ttk.Label(self.output_frame, cursor='hand2',
-                            wraplength=500,
+                            wraplength=350,
                             textvariable=self.sc_link, style='Link.TLabel',
-                            compound=LEFT, image=self.sc_img)
-        self.sc.pack(anchor='w', pady=20)
+                            compound=LEFT)
+        self.sc.grid(row=1, column=1, sticky='w', pady=10)
         self.sc.bind('<Button-1>', lambda e: self.link_callback(e, self.sc_link))
 
         self.am = ttk.Label(self.output_frame, cursor='hand2',
-                            wraplength=500,
+                            wraplength=350,
                             textvariable=self.am_link, style='Link.TLabel',
-                            compound=LEFT, image=self.am_img)
-        self.am.pack(anchor='w', pady=20)
+                            compound=LEFT)
+        self.am.grid(row=2, column=1, sticky='w', pady=10)
         self.am.bind('<Button-1>', lambda e: self.link_callback(e, self.am_link))
 
     def callback(self, song, artist):
