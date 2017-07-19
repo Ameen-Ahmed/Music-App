4178a30406d1803878aa4b78a10218db8a33d8e8
Ameen Ahmed <ameenahmed39@gmail.com>
2017-07-19 18:50:00 -0400

    Specify lxml parser for BeautifulSoup constructor

--- Web.py
+++ Web.py
@@ -7,9 +7,9 @@ def youtube_search(song, artist):
     format_song = song.replace(' ', '+').strip()
     format_artist = artist.replace(' ', '+').strip()
     data = request.urlopen('https://www.youtube.com/results?search_query=' + format_song + '+by+' + format_artist)
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, 'lxml')
     for link in soup.findAll('a', attrs={'href': re.compile('/watch', re.I)}):
-        return link['href']
+        return 'https://www.youtube.com' + link['href']
 
 
 def soundcloud_search(song, artist):
@@ -18,7 +18,7 @@ def soundcloud_search(song, artist):
     x = song.replace(' ', '-')
     y = artist.replace(' ', '-')
     data = request.urlopen('https://soundcloud.com/search?q=' + format_song + '+' + format_artist)
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, 'lxml')
     for link in soup.findAll('a', attrs={'href': re.compile('(' + x + '|' + y + ')', re.I)}):
         return 'https://www.soundcloud.com' + link['href']
 
@@ -27,6 +27,6 @@ def audiomack_search(song, artist):
     format_song = song.replace(' ', '+').strip()
     format_artist = artist.replace(' ', '+').strip()
     data = request.urlopen('https://www.audiomack.com/search?q=' + format_song + '+' + format_artist)
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, 'lxml')
     for link in soup.findAll('a', attrs={'href': re.compile('/song/.*?' + format_song, re.I)}):
         return 'https://www.audiomack.com' + link['href']
